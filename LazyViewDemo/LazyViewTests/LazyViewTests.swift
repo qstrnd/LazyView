40981d3eeecc9c8f2d0170efc5341b11a3ce24dd
Andy <a.iakovlev@proton.me>
2023-12-08 02:00:39 +0400

    Add basic unit tests

--- LazyViewDemo/LazyViewTests/LazyViewTests.swift
+++ LazyViewDemo/LazyViewTests/LazyViewTests.swift
@@ -6,30 +6,97 @@
 //
 
 import XCTest
+import LazyView
 
 final class LazyViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var sut: LazyView<UILabel>!
+    var containerView: ContainerView!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    override func setUp() {
+        let lazyView = LazyView {
+            UILabel()
         }
+
+        containerView = ContainerView()
+        containerView.lazyViewContainerConfiguration = LazyViewContainerConfiguration(container: containerView) {
+            lazyView
+        }
+
+        sut = lazyView
+    }
+
+    // MARK: Init
+
+    func testThatPostInitIsCalledOnlyOnceAfterInitialization() {
+        var times = 0
+
+        sut.postInitHandler = { _ in
+            times += 1
+        }
+
+        sut.configure(on: true)
+        sut.configure(on: true)
+
+        XCTAssertEqual(times, 1)
+        XCTAssertNotNil(sut.asUIView)
+    }
+
+    func testThatPostInitIsNotCalledWhenNoInitializationHappened() {
+        var times = 0
+
+        sut.postInitHandler = { _ in
+            times += 1
+        }
+
+        sut.configure(on: false)
+
+        XCTAssertEqual(times, 0)
+        XCTAssertNil(sut.asUIView)
+    }
+
+    // MARK: Configuration
+
+    func testThatPostConfigurationIsCalledOnlyOnInitializedView() {
+        var times = 0
+
+        sut.postConfigureHandler = { _, _ in
+            times += 1
+        }
+
+        sut.configure(on: false)
+        sut.configure(on: true) // first time postConfigure called
+        sut.configure(on: true)
+        sut.configure(on: false)
+
+        XCTAssertEqual(times, 3)
+        XCTAssertNotNil(sut.asUIView)
+    }
+
+    // MARK: Operations
+
+    func testThatOperationIsPerformedOnlyOnInitializedView() {
+        var times = 0
+
+        // not called
+        sut.performIfVisible { _ in
+            times += 1
+        }
+
+        sut.configure(on: true)
+
+        // called from now on
+
+        sut.performIfVisible { _ in
+            times += 1
+        }
+
+        sut.performIfVisible { _ in
+            times += 1
+        }
+
+        XCTAssertEqual(times, 2)
+        XCTAssertNotNil(sut.asUIView)
     }
 
 }
