f5c414e8d485d846fbdfa5da1156155c0115bf72
Andy <a.iakovlev@proton.me>
2023-12-08 00:36:58 +0400

    Add documentation in comments

--- LazyView/Classes/LazyView.swift
+++ LazyView/Classes/LazyView.swift
@@ -15,29 +15,63 @@ public protocol LazyViewReference: AnyObject {
     func prepare()
 }
 
+/// A wrapper for lazily initializing any view. It's initialized with a closure that's executed when the view needs to be configured for the first time.
+///
+/// Trigger view configuration by calling 
+/// ```
+/// configure(on condition: Bool, _ configureOperation: ViewOperation)
+/// ```
+/// and it's relatives.
+///
+/// Use `postInitHandler` for any configuration that needs to be done only once immediately after initialization (like setting your view's `delegate`).
+///
+/// Use `postConfigureHandler` for tweaking any post-configuration logic if the view has been initialized. 
+/// The default handler sets `isHidden` to true if configuration condition is not met and vice versa.
+///
+/// - Important: There's no compile-time check of what you put inside as an underlying view since your views can be referenced as protocols.
+/// Nevertheless, keep in mind that attempting to insert an object that is not a UIView into the view hierarchy will trigger an `assertionFailure`.
 public final class LazyView<View>: LazyViewReference {
     public typealias Initializer = () -> View
     public typealias ViewCondition = (View) -> Bool
     public typealias ViewOperation = (View) -> Void
     public typealias PostConfiguration = (_ view: View, _ isConfigured: Bool) -> Void
 
-
     // MARK: - Properties
-
+    
+    /// The container for the lazy view that is responsible for adding the view to the view hierarchy.
+    /// - Note: Do not set it manually! Instead, reference your lazy view wrapper when initializing `LazyViewContainerConfiguration`
     public weak var container: LazyViewContainer?
+
+    /// Handler that is called only once immediately after initialization. Use it for additonal configuration of your view like setting its `delegate`
+    /// - Warning: Do not forget to reference `self` as `weak` to avoid a reference cycle
     public var postInitHandler: ViewOperation?
 
-    /// Default configuration is provided
+    /// Handler for any post configuration
+    ///
+    /// Namely, the handler is called in two scenarios:
+    /// 1. The view is successfully configured (the configuration condition is met).
+    /// 2. The view is not configured because the condition is not met, but it had already been initialized. In this case, you may want to add some cleanup logic.
+    ///
+    /// - Note: If you don't set this property, a default handler is provided. The default handler sets `isHidden` to true if the configuration condition is not met and vice versa.
+    /// - Warning: Do not forget to reference `self` as `weak` to avoid a reference cycle.
     public var postConfigureHandler: PostConfiguration?
-
-    public var uniqueViewId = UUID()
-
+    
+    /// The id of the lazy used to match the view with its configuration in `LazyViewContainerConfiguration`
+    public let uniqueViewId = UUID()
+    
+    /// Get the underlying view as `UIView`
+    /// - Note: This property returns the underlying view only if it has been initialized. If you want to force initialize the view, use `configureAsVisible` instead.
     public var asUIView: UIView? {
         guard let view = view else { return nil }
-        return view as? UIView
+        guard let uiView = view as? UIView else {
+            assertionFailure("Failed to cast the underlying view to UIView")
+            return nil
+        }
+        return uiView
     }
 
-    /// Get underlying view if it has been already initialized
+    /// Get the underlying view
+    /// - Note: This property returns the underlying view only if it has been initialized. If you want to force initialize the view, use `configureAsVisible` instead.
     public var view: View? {
         switch state {
         case .uninitialized:
@@ -62,7 +96,14 @@ public final class LazyView<View>: LazyViewReference {
     // MARK: - Methods
 
     // MARK: Init
-
+    
+    /// Initialize the lazy view with a closure
+    ///
+    /// - Parameter initializer: The closure that return an initialized `View`.
+    /// - Warning: Do not forget to reference `self` as `weak` to avoid a reference cycle.
+    /// - Tip: A recommended approach is to avoid referencing `self` in the initializer to prevent the error `self used before super.init call`.
+    /// Instead, reference `self` in `postConfigureHandler` that is set after finishing your container's initialization.
+    /// This way, you can avoid the necessity of using force unwrapping for `LazyView` properties.
     public init(_ initializer: @escaping Initializer) {
         self.state = .uninitialized(initializer)
 
@@ -71,7 +112,9 @@ public final class LazyView<View>: LazyViewReference {
             view.isHidden = !isConfigured
         }
     }
-
+    
+    /// Prepare the underlying view for usage by initializing and inserting it into the view hierarchy.
+    /// Avoid calling this method manually; instead, use the `configure(...)` family of methods.
     public func prepare() {
         initialize()
         insertAsSubview()
@@ -79,15 +122,20 @@ public final class LazyView<View>: LazyViewReference {
 
     // MARK: Configuration
 
+    /// Use this variation of `configure` method when you don't need to provide variable configuration logic for different states of your view.
     public func configure(on condition: Bool) {
         configure(on: condition, { _ in })
     }
-
-    public func configureAsVisible(_ configureOperation: ViewOperation = { _ in }) {
-        configure(on: true, configureOperation)
-    }
-
-    public func configure(on condition: Bool, _ configureOperation: ViewOperation) {
+    
+    /// Configure the underlying view if condition is met.
+    ///
+    /// - Parameters:
+    ///   - condition: The condition that determines if the view needs to be configured
+    ///   - configureOperation: Configuration closure used to update your view with the appropriate state.
+    ///
+    /// - Note: This method triggers view initialization before the first configuration when the specified condition is met.
+    /// After initialization, `postConfigureHandler` is called on every configuration attempt.
+    public func configure(on condition: Bool = true, _ configureOperation: ViewOperation) {
         guard condition else {
             if case .ready(let view) = state {
                 postConfigureHandler?(view, false)
@@ -114,7 +162,10 @@ public final class LazyView<View>: LazyViewReference {
     }
 
     // MARK: Operations
-
+    
+    /// Perform an operation on the underlying view if it is visible (and has been initialized).
+    ///
+    /// - Parameter operation: The operation on the view
     public func performIfVisible(_ operation: ViewOperation) {
         perform(
             on: { _ in
@@ -124,7 +175,12 @@ public final class LazyView<View>: LazyViewReference {
             operation
         )
     }
-
+    
+    /// Perform an operation on the underlying view if it has been initialized.
+    ///
+    /// - Parameters:
+    ///   - condition: The condition that must be met for the operation to be performed.
+    ///   - operation: The operation on the view.
     public func perform(on condition: ViewCondition, _ operation: ViewOperation) {
         guard case .initialized(let view) = state else {
             return
--- LazyView/Classes/LazyViewContainerConfiguration.swift
+++ LazyView/Classes/LazyViewContainerConfiguration.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
+/// The configuration that describle the view hierarchy inside the `LazyViewContainer`.
+/// It specifies the exact place where `LazyView` needs to be inserted.
+///
+/// Use special DSL to initialize its instance as in the example below:
+/// ```
+/// lazyViewContainerConfiguration = LazyViewContainerConfiguration(container: self) {
+///     stackView.containing {
+///         label
+///         nestedLazyStack.containing {
+///             button1
+///             button2
+///         }
+///         textField
+///     }
+/// }
+/// ```
+/// - Important: With this DSL, you can specify parent-child relationships using `containing` and sibling relationships otherwise.
+/// `LazyView` can be nested at any level, but a `UIView` can only be placed at the topmost level, and you'll get a compile-time error if you attempt otherwise.
+/// Instances of `UIView` are not inserted into a superview automatically, so you need to set them up preliminarily.
 public struct LazyViewContainerConfiguration {
     public typealias ViewId = UUID
     public typealias RelationsDictionary = [ViewId: Relations]
@@ -41,7 +60,12 @@ public struct LazyViewContainerConfiguration {
     init(relations: [ViewId : Relations]) {
         self.relations = relations
     }
-
+    
+    /// Initialize the configuration.
+    ///
+    /// - Parameters:
+    ///   - container: Provide the container so it can be set for every `lazyView` specified in the configuration.
+    ///   - builder: DSL for describing view hierarchy. Read more in the documentation for `LazyViewContainerConfiguration`.
     public init(
         container: LazyViewContainer,
         @LazyViewContainerConfigurationBuilder _ builder: () -> LazyViewContainerConfigurationBuilderResult
